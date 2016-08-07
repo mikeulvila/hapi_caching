026b8d7395c04233c4986c548cb8f78def8f2c09
Mike Ulvila <mikeulvila@me.com>
2016-08-06 21:48:33 -0500

    create route for .png file

--- routes/caching.js
+++ routes/caching.js
@@ -5,6 +5,20 @@ const crypto = require('crypto');
 
 exports.register = function (server, options, next) {
 
+  const _createHash = (input) => {
+
+    const jsonString = JSON.stringify(input);
+
+    return crypto.createHash('sha1').update(jsonString).digest('hex');
+  };
+
+  server.route({
+    method: 'GET',
+    path: '/logo.png',
+    handler: {
+      file: './public/hapi_logo.png'
+    }
+  });
 
   return next();
 };
